902691cc9d5b7a384db7c5f7c3544dbea2d8b29e
ed55567 <emmanueldaniels37@gmail.com>
2023-07-08 07:31:15 -0400

    added const.py and main.py files and added  init and mainloop methods

---
+++ src/const.py
@@ -0,0 +1,8 @@
+# Screen Dimensions
+Width = 800
+Height = 800
+
+# Board Dimensions
+ROWS = 8
+COLS = 8
+SQ_SIZE = Width // COLS
---
+++ src/main.py
@@ -0,0 +1,11 @@
+
+class Main:
+
+    def __init__(self):
+        print("Main init")
+
+    def mainloop(self):
+        print("Main mainloop")
+
+main = Main()
+main.mainloop()
