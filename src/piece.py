12f0a03f86e43e36ae5f2bb3be65ccbfa9f0e7ba
ed55567 <emmanueldaniels37@gmail.com>
2023-07-09 22:42:41 -0400

    added board and piece py files

---
+++ src/piece.py
@@ -0,0 +1,12 @@
+from const import *
+
+class Piece:
+
+    def _init_(self, name, color, value, texture, texture_rect=None):
+        pass
+
+    class Pawn(Piece):
+        
+
+        def _init_(self, color):
+            pass
