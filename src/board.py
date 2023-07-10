961040aadb0a899e6ca04582ccdc367e35ee2175
ed55567 <emmanueldaniels37@gmail.com>
2023-07-10 16:20:10 -0400

    added two dimensional array in self init

--- src/board.py
+++ src/board.py
@@ -5,14 +5,10 @@ from square import Square
 class Board:
 
     def __init__(self):
-        self.squares = []
+         self.squares = [[0, 0, 0, 0, 0, 0, 0, 0] for col in range(COLS)]
 
-        self._create()
     
-    # def _create(self):
-    #     self.squares = [[0, 0, 0, 0, 0, 0, 0, 0] for col in range(COLS)]:
-    #      pass
-
+    def _create(self):
         for row in range(ROWS):
             for col in range(COLS):
                 self.squares[row][col] = Square(row, col)
