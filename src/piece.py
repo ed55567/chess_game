8bbe5cd80219e2040401d75a440216dda4b4c9b7
ed55567 <emmanueldaniels37@gmail.com>
2023-07-10 16:54:17 -0400

    added pieces.py classes

--- src/piece.py
+++ src/piece.py
@@ -3,10 +3,50 @@ from const import *
 class Piece:
 
     def _init_(self, name, color, value, texture, texture_rect=None):
-        pass
+        self.name = name
+        self.color = color
+        value_sign = 1 if color == 'white' else -1
+        self.value = value * value_sign
+        self.set_texture()
+        self.texture_rect = texture_rect
+
+    # Pawn class
 
     class Pawn(Piece):
         
-
         def _init_(self, color):
-            pass
+            if color == 'white':
+                self.dir = -1 # white pawns move up the board
+            else:
+                self.dir = 1  # black pawns move down the board
+                super()._init_('pawn', color, 1.0 ) # value moves for pawn is 1.0
+
+    # Knight class
+     
+    class Knight(Piece):
+        def _init_(self, color):
+            super()._init_('knight', color, 3.0) # value for knight is 3.0
+
+    #Bishop class
+    
+    class Bishop(Piece):
+        def _init_(self, color):
+            super()._init_('bishop', color, 3.0) # value for bishop is 3.0
+
+    #Rook class
+     
+    class Rook(Piece):
+        def _init_(self, color):
+            super()._init_('rook', color, 5.0) # value for rook is 5.0
+
+    #Queen class
+
+    class Queen(Piece):
+        def _init_(self, color):
+            super()._init_('queen', color, 9.0) # value for queen is 9.0
+
+    #King class
+
+    class King(Piece):
+        def _init_(self, color):
+            super()._init_('king', color, 100000.0) # value for king is 0.0
