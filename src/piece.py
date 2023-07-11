e6cd95a8f9fc2621e311eda432a817b3ffe77b7f
ed55567 <edblade555@gmail.com>
2023-07-11 00:49:45 -0400

    added images and more pieces set texture

--- src/piece.py
+++ src/piece.py
@@ -7,8 +7,18 @@ class Piece:
         self.color = color
         value_sign = 1 if color == 'white' else -1
         self.value = value * value_sign
+        self.moves = []
+        self.moved = False
+        self.texture = texture
         self.set_texture()
         self.texture_rect = texture_rect
+
+    def set_texture(self, size=80):
+        self.texture = os.path.join(
+            f'assets/images/imgs-{size}px/{self.color}_{self.name}.png')
+        
+    def add_move(self, move):
+        self.moves.append(move)
 
     # Pawn class
 
