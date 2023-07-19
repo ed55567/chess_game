5434e2dfe4948d7603a94a536121c49ecc27f812
ed55567 <edblade555@gmail.com>
2023-07-19 00:19:07 -0400

    added more pieces to the board

--- src/board.py
+++ src/board.py
@@ -1,5 +1,6 @@
 from const import *
 from square import Square
+from piece import Piece
 
 
 class Board:
@@ -15,5 +16,25 @@ class Board:
                 self.squares[row][col] = Square(row, col)
 
     def _add_pieces(self, color):
-         if color == 'white':
-             pawn_row, row_other = (6, 7)
+        row_pawn, row_other = (6, 7) if color == 'white' else (1, 0)
+
+        #pawns
+        self.squares[row_pawn][col] = Square(row_pawn, col, Piece.Pawn(color))
+
+        #kinghts
+        self.squares[row_other][1] = Square(row_other, 1, Piece.Knight(color))
+        self.squares[row_other][6] = Square(row_other, 6, Piece.Knight(color))
+
+        #bishops
+        self.squares[row_other][2] = Square(row_other, 2, Piece.Bishop(color))
+        self.squares[row_other][5] = Square(row_other, 5, Piece.Bishop(color))
+
+        #rooks
+        self.squares[row_other][0] = Square(row_other, 0, Piece.Rook(color))
+        self.squares[row_other][7] = Square(row_other, 7, Piece.Rook(color))
+
+        #queen
+        self.squares[row_other][3] = Square(row_other, 3, Piece.Queen(color))
+
+        #king
+        self.squares[row_other][4] = Square(row_other, 4, Piece.King(color))
